ca4ac929e65542ddb17b87801853343560e36b83
Manoj kumar <manoj.bhoosani@gmail.com>
2023-02-17 00:24:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([])
+  const APP_ID = "33042e6e";
+  const APP_KEYS = "9b0755b710b1ed633456a0fd5c691e48";
+
+  //  const example_request =
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEYS}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" />
+        <button type="submit"> Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
